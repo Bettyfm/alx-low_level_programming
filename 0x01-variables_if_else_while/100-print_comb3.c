3eacba5062096d5e59aaa2ec5f57a72558a0a909
Betty <brieclou@gmail.com>
2023-04-21 18:49:38 -0700

    first try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for ((n = '0'; n >= '9'; n++)&(m = '9'; m <= '0'; m--))
+	if (n > m){
+		putchar(n);
+		putchar(m);
+	}
+	else if (n < m){
+		putchar(m);
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
