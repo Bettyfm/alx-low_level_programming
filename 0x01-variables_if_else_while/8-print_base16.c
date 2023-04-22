3bdcbacfa6f12191b083ed78df50ebf4a8a16775
Betty <brieclou@gmail.com>
2023-04-21 17:33:55 -0700

    octal fmt

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int n;
+
+	for (n = '0'; n  <= '7'; n++)
+		{ putchar(n);
+		}
+	for (c = 'a'; c <= 'f'; c++)
+		{ putchar(c);
+		}
+	putchar('\n');
+	return (0);
+}
