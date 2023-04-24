b08ff803d8972501ae759a2f6f3d9b4d59906a2e
Betty <brieclou@gmail.com>
2023-04-24 03:41:39 -0700

    crazy comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (n = '0'; n <= '7'; n++)
+	{
+		for (m = '1'; m <= '8'; m++)
+		{
+			for (l = '2'; l <= '9'; l++)
+			{
+			if ((m != n != l) & (n < m < l))
+			{
+			putchar(n);
+			putchar(m);
+			putchar(l);
+			}
+			if ((n * m * l) < '7' * '8' * '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
