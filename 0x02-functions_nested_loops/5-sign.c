027ff7e216f1dee8841634c87ca0b20190d69581
Betty <brieclou@gmail.com>
2023-04-25 09:07:22 -0700

    print sign of numb

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of an int
+ * @n: interger to be signed
+ *
+ * Return: 1 for greater than zero -1 for less than zero and 0 for zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	_putchar('+');
+	return (1);
+	else if (n == 0)
+	_putchar('0');
+	return (0);
+	else if (n < 0)
+	_putchar('-');
+	return (-1);
+}
