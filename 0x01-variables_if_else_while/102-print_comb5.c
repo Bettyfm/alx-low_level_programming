1013952c78e3e36aa752537c38d57a3eadabb718
Betty <brieclou@gmail.com>
2023-04-24 19:58:12 -0700

    comb putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	while (n <= 99)
+	{
+		n = m
+		while (m <= 99)
+		{
+			if (n != m)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((n / 10) + 48);
+
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			m++;
+		}
+		n++
+	}
+	putchar('\n');
+	return (0);
+}
