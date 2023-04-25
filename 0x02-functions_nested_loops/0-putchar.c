9ae1ed314c45e6e4886e61ac8453811e45c7a0bb
Betty <brieclou@gmail.com>
2023-04-25 04:04:17 -0700

    first try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	Putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
