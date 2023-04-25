32d5c9a665b90a829d0a5aba436a8344185d6888
Betty <brieclou@gmail.com>
2023-04-25 07:22:22 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints alphabet 10 times
+ *
+ * parameter: void function no parameter
+ *
+ * Return: void function no return
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
