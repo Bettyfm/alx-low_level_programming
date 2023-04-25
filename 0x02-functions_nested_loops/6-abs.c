656ca97aa7f71903889dddd67d5c7e846927d8f7
Betty <brieclou@gmail.com>
2023-04-25 10:34:16 -0700

    absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function for absolute value
+ *
+ * @int: integer for abs
+ *
+ * Return: absolute value of int
+ */
+int _abs(int)
+{
+	int ab;
+
+	if (int < 0)
+	ab = int * -1
+	return (ab);
+	else
+	_putchar('int');
+	return (0);
+}
