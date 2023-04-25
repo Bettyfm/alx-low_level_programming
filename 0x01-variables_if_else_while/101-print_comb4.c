f3d71516eaac5b04c7cf8b77cbeb2388c0309096
Betty <brieclou@gmail.com>
2023-04-24 18:31:32 -0700

    combinations with putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,15 +16,17 @@ int main(void)
 	{
 		for (m = '1'; m <= '8'; m++)
 		{
-			for (l = '2'; l <= '9'; l++)
+		for (l = '2'; l <= '9'; l++)
 			{
-			if (((m != n) != l) & ((n < m) < l))
+		if (n < m && m < l)
 			{
 			putchar(n);
 			putchar(m);
 			putchar(l);
 			}
-			if ((n * m * l) < '7' * '8' * '9')
+		if (n == '7' && m == '8' && l == '9')
+				break;
+		else
 			{
 			putchar(',');
 			putchar(' ');
