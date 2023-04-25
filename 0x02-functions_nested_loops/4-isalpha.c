4d1ebb4c047c2aa88ca788db11710c182451dafc
Betty <brieclou@gmail.com>
2023-04-25 08:32:15 -0700

    determine upper and lower case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - function to determine upper or lower case
+ *
+ * @c: parameter to evaluate
+ *
+ * Return: 1 if c is upper or lowercase 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a') & (c >= 'z') & (c >= 'A') & (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
