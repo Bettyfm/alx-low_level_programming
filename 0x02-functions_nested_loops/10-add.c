51bf57e573c80ed37b8ba970186adac0d0003c20
Betty <brieclou@gmail.com>
2023-04-25 19:46:02 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - function that add two int
+ *
+ * @n: first parameter to be added
+ * @m: second parameter to be added
+ *
+ * Return: l value of sun
+ */
+int add(int n, int m)
+{
+	int l;
+
+	l = n + m;
+	return (l);
+}
