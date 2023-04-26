bf725339eaeecc4abbd5864b049ae85df10aac63
Betty <brieclou@gmail.com>
2023-04-26 09:18:29 -0700

    idk

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /* not incrementing @i */
+	while (i < 10)
 	{
-		putchar(i);
+		putchar(i); /* not incrementing @i */
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
