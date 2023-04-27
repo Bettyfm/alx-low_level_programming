8ccfde0a0e8e66b80077ffc30c5688cfe2ac792a
Betty <brieclou@gmail.com>
2023-04-27 04:23:16 -0700

    rt

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,17 +11,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b) && (b > c))
+	if (a < b)
 	{
-		largest = a;
+		if (c < b)
+		{
+			largest = b;
+		}
+		else 
+			largest = c;
 	}
-	else if ((b > a) && (a > c))
-	{
-		largest = b;
-	}
-	else if ((c > a) && (a > b))
-	{
-		largest = c;
-	}
+	else largest = a;
 	return (largest);
 }
