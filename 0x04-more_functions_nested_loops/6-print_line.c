352321706e84a9af58a3d243547c4c33bb31ac6a
Betty <brieclou@gmail.com>
2023-04-27 13:41:05 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - function to print line
+ * @n: number of lines
+ */
+void print_line(int n)
+{
+	if (n > 0)
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	else
+		_putchar('\n');
+
+}
