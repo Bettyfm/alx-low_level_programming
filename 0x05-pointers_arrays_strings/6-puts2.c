7434d9cff7e57708532308f700041055424d2aa2
Betty <brieclou@gmail.com>
2023-05-02 19:19:15 -0700

    print even index number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints even numbers
+ * @str: string
+ * no return
+ */
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
