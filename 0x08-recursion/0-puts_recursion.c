b11b04ccc77abc2952ccf2ccc29475d2f64168ff
Betty <brieclou@gmail.com>
2023-05-11 14:57:48 -0700

    im back

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints puts
+ * @s: string
+ * void funbction no return
+ */
+void _puts_recursion(char *s)
+{
+	while (s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
