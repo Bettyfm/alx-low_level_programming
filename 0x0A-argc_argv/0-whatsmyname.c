31825424f651cd68313f3aea6a4f53795732feb0
Betty <brieclou@gmail.com>
2023-05-12 18:29:41 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: arg count 
+ * @argv: name of arguments
+ * Return: always 0 if success
+ */
+int main(int argc , char *argv[])
+{
+	if argc = 0;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
+
