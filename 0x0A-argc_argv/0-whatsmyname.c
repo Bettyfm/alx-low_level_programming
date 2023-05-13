c5f1bcb0e7cf142a7276fd1c14d8e3863acef6a2
Betty <brieclou@gmail.com>
2023-05-12 18:41:38 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Entry point
- * @argc: arg count 
+ * @argc: arg count
  * @argv: name of arguments
  * Return: always 0 if success
  */
-int main(int __attribute__((unused)) argc , char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
+
 	return (0);
 
 }
