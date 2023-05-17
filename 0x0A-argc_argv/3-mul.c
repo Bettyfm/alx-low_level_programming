8f31a123a6694f607239d86bd93994a95b3a5cff
Betty <brieclou@gmail.com>
2023-05-17 13:14:13 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	while (argc <3)
+	if (argc < 3)
 	{
-		mul = atoi(argv[1]) * atoi (argv[2]);
+		mul = atoi(argv[1]) * atoi(argv[2]);
 			printf("%d\n", mul);
 			return (0);
 	}
-	else 
+	else
 	{
 		printf("error\n");
 	return (1);
