f7fe2d1db5c17d4197928ed7afaa9b02721b0814
Betty <brieclou@gmail.com>
2023-05-17 18:44:15 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -11,24 +12,27 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int add = 0;
+	long unsigned int j;
+	int add;
+	char *a;
 
-	if (argc == 1)
-	{
+	if (argc == 0)
 		printf("0\n");
-	}
 	else
 	{
-		for (i = 0; i < argc; i++)
-			if (isdigit(atoi(argv[i])) != 0)
+	for (i = 0; i < argc; i++)
+	{
+		a = argv[i];
+
+		for (j = 0; j < strlen(a); j++)
+			if (a[j] < '0' || a[j] > '9')
 			{
-				add = add + atoi(argv[i]);
+				return (printf("Error\n"), 1);
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+		add = add + atoi(a);
+		a++;
+	}
+	printf("%d\n", add);
 	}
 	return (0);
 }
