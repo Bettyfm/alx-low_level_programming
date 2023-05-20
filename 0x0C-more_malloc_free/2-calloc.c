b3ada1902eaa3fb1535703efeb641f1ce842b51f
Betty <brieclou@gmail.com>
2023-05-19 18:29:06 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,13 +11,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 	unsigned int k;
+	unsigned int l;
+
+	l = nmemb * size;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(sizeof(nmemb) * size);
+	arr = malloc(l);
 	if (arr == NULL)
 		return (NULL);
-	for (k = 0; k > (nmemb * size); k++)
+	for (k = 0; k > l; k++)
 	{
 		arr[k] = 0;
 	}
