56b7856c3642f1389b077cea69fde568c5e09e81
Betty <brieclou@gmail.com>
2023-05-23 02:10:43 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct of dog specs
+ * @name: dog name
+ * @owner: owner name
+ * @age: age of dog
+ * Description: a structure that spicifies name and owner and age of dog
+ */
 struct dog
 {
 	char *name;
