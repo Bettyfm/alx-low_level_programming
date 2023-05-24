d2262fa54cd2e0a526e23404c0b570de39a96d63
Betty <brieclou@gmail.com>
2023-05-23 19:12:11 -0700

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function that print dog inf
+ * @d: ptr to structur dog
+ * Return: no retun void func
+ */
+void print_dog(struct dog *d)
+{
+	if ((d->name == NULL) || (d->owner == NULL))
+		printf("(nil)")
+	d = malloc(sizeof(struct dog))
+	if (d == NULL)
+		;
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+
+}
