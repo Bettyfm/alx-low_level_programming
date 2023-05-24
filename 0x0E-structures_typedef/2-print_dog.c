59b323899abc54062f4640ed5a40131c8ad4b0b9
Betty <brieclou@gmail.com>
2023-05-23 19:17:20 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * print_dog - function that print dog inf
  * @d: ptr to structur dog
@@ -10,9 +10,9 @@ void print_dog(struct dog *d)
 {
 	if ((d->name == NULL) || (d->owner == NULL))
 		printf("(nil)");
-	d = malloc(sizeof(struct dog))
+	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		;
+		return;
 	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 
 }
