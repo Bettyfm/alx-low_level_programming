a7d549f1f09b921a4b833c3c30e7c1aa05e6845f
Bettyfm <bettyfraigui@gmail.com>
2023-06-30 18:16:57 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h != 0)
 	{
-		if (str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
-		printf("[%d]%s\n", h.len, h->str);
+		printf("[%d]%s\n", h->len, h->str);
 		i++;
 		h = h->next;
 	}
