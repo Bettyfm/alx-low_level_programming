ebb9f3c53c18a2c4f0f1c5ab92147750906b82ee
Bettyfm <bettyfraigui@gmail.com>
2023-07-01 18:42:56 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,12 +5,24 @@
 /**
  * add_node - function that adds a node
  * @head: ptr to first node
- * str: string to be copied
+ * @str: string to be copied
  * Return: struct node
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	list_t *NewNode = malloc(sizeof(list_t));
+	unsigned int len = 0;
+
+	while (*str)
+		len++;
+	if (NewNode == NULL)
+		return (NULL);
+	NewNode->str = strdup(str);
+	NewNode->len = len;
+	NewNode->next = *head;
+	*head = NewNode;
+
+	return (*head);
 
 
 
