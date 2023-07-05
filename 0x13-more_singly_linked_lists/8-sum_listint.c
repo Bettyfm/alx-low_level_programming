a17ea256a5984de7183775bad29d2b3bc62d9a60
Bettyfm <bettyfraigui@gmail.com>
2023-07-04 18:07:43 -0700

    task8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,5 +3,18 @@
 #include <stdlib.h>
 
 /**
+ * sum_listint - func that sum
+ * @head: first node
+ * Return: sum of all data or 0
  */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
 
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
