8955919656d46117068bb22ef6f4a3f4007f1a29
Bettyfm <bettyfraigui@gmail.com>
2023-07-04 17:59:01 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,5 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * get_nodeint_at_index - func to get node at index
+ * @head: first node
+ * @index: node number
+ * Return: ptr or NULL
  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int n;
+	listint_t *node;
 
+	node = head;
+	while ((node != NULL) && (n < index))
+	{
+		node = node->next;
+		n++;
+	}
+	if (node)
+		return (node);
+	else
+		return (NULL);
+
+
+}
