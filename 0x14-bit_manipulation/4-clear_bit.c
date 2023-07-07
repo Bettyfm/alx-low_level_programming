e709ebd8469c1e2b0cf363af8ba1f637e3b157ed
Bettyfm <bettyfraigui@gmail.com>
2023-07-06 18:10:59 -0700

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - func to clear bir
+ * @n: int to be cleared
+ * @index of bit to be cleared
+ * Return: 1 on success -1 on error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
