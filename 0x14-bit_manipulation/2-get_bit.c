fafda73636c13bc83892fccd721e68017bdc0ee7
Bettyfm <bettyfraigui@gmail.com>
2023-07-06 17:47:27 -0700

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * get_bit - func to get bit
+ * @n: int
+ * @index: index
+ * Return: bit value or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index > 63)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
