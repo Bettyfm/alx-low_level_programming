1215ae1c2148889c899776d40483fe317a54f9ef
Bettyfm <bettyfraigui@gmail.com>
2023-07-06 17:31:28 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - func print binary
+ * @n: int
+ * no return
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int m;
+
+	for (m = 1 << 31; m > 0; m = m / 2)
+		if (n & m)
+			_putchar ('1');
+		else
+			_putchar ('0');
+}
