df00317efdccbe68e6608ab7f64bc1393696c1d1
Bettyfm <bettyfraigui@gmail.com>
2023-07-10 18:30:58 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 
 /**
- * create_file - creates a file 
- * @filename: name of the file
- * @text_content: content of file
+ * append_text_to_file - func to add data to file
+ * @filename: name of file to add data to
+ * @text_content: str to be added
  *
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int bytw = 0;
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
