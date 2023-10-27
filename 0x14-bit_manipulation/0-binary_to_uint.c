368ae272a5957e6f541ced83d6e9d933ea052d72
Bettyfm <bettyfraigui@gmail.com>
2023-10-26 18:01:48 -0700

    tasks 1to 4

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - bin to int
+ * @b: char bin
+ * Return: 0 fail and num on success
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if ((*b != '0') && (*b != '1'))
+			return (0);
+		num = 2 * num + (*b - '0');
+		b++;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bit manipuulation
