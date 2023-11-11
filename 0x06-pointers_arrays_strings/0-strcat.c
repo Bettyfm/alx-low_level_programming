dff5a2145ddd2c94de053e5fd5105a54b0587e9a
Bettyfm <bettyfraigui@gmail.com>
2023-11-11 01:43:08 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,41 @@
 #include "main.h"
 
 /**
- * *_strcat
+ * _strcat - string concatinate func
+ * @dest: string destination of concate
+ * @src: string source to be concatinated
+ * Return: ptr to final string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+
+	n = _strlen(dest);
+
+	for (m = 0; src[m]; m++)
+	{
+		dest[n] = src[m];
+			n++;
+	}
+
+}
+
+/**
+ * _strlen - function that print the length of string
+ * @s: string char
+ *
+ * Return: lenght (success)
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght = lenght + 1;
+		s = s + 1;
+	}
+	return (lenght);
+}
